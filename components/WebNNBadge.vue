cd7598acf98f3f5df2a154b12e7ad5a0161d657b
Zhang, Belem <belem.zhang@intel.com>
2019-12-31 15:49:01 +0800

    init

--- components/WebNNBadge.vue
+++ components/WebNNBadge.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="webnnbadge">
+    <div id="flip">
+      <div><div>5 people online</div></div>
+      <div><div>lifeStyle</div></div>
+      <div><div>Everything</div></div>
+    </div>
     <span v-if="webmlstatus" class="webnn-supported">
       Supported
     </span>
@@ -45,5 +50,67 @@ export default {
 
 .webnn-not-supported {
   background-color: rgb(255, 144, 204);
+}
+
+#flip {
+  height: 50px;
+  overflow: hidden;
+}
+
+#flip > div > div {
+  color: #fff;
+  padding: 4px 12px;
+  height: 45px;
+  display: inline-block;
+}
+
+#flip div:first-child {
+  animation: show 5s linear infinite;
+}
+
+#flip div div {
+  background: #42c58a;
+}
+
+#flip div:first-child div {
+  background: #4ec7f3;
+}
+
+#flip div:last-child div {
+  background: #dc143c;
+}
+
+@keyframes show {
+  0% {
+    margin-top: -270px;
+  }
+
+  5% {
+    margin-top: -180px;
+  }
+
+  33% {
+    margin-top: -180px;
+  }
+
+  38% {
+    margin-top: -90px;
+  }
+
+  66% {
+    margin-top: -90px;
+  }
+
+  71% {
+    margin-top: 0px;
+  }
+
+  99.99% {
+    margin-top: 0px;
+  }
+
+  100% {
+    margin-top: -270px;
+  }
 }
 </style>
